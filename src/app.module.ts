e74e18413c72cda4545beaebe1a601eb88f646a3
Akeuuu <leo.ferrarini@epitech.eu>
2026-04-07 16:29:03 +0200

    swagger nestJs added on localhost:3000/api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+  import { Module } from '@nestjs/common';
+  import { MongooseModule } from '@nestjs/mongoose';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  @Module({
+    imports: [
+      MongooseModule.forRoot('mongodb://mongodb:27017/blog_db'),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {}
